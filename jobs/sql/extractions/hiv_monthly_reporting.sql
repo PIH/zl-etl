d906495b6f672ccc322ed39123d08723704b3741
dmdesimone <ddesimone@pih.org>
2022-03-29 20:58:18 +0200

    Update hiv_monthly_reporting.sql

--- jobs/sql/extractions/hiv_monthly_reporting.sql
+++ jobs/sql/extractions/hiv_monthly_reporting.sql
@@ -15,32 +15,31 @@ FROM patient_program WHERE voided=0 AND program_id = @hiv_program_id;
 DROP TEMPORARY TABLE IF EXISTS temp_eom_appts;
 CREATE TEMPORARY TABLE temp_eom_appts
 (
-monthly_reporting_id			INT(11) AUTO_INCREMENT,
-patient_id						INT(11),
-patient_program_id				INT(11),
-date_enrolled					DATETIME,
-date_completed					DATETIME,
-reporting_date					DATE,
+monthly_reporting_id		INT(11) AUTO_INCREMENT,
+patient_id			INT(11),
+patient_program_id		INT(11),
+date_enrolled			DATETIME,
+date_completed			DATETIME,
+reporting_date			DATE,
 latest_hiv_note_encounter_id	INT(11),
-latest_hiv_visit_date			DATETIME,
+latest_hiv_visit_date		DATETIME,
 latest_expected_hiv_visit_date	DATETIME,
-hiv_visit_days_late				INT,
+hiv_visit_days_late		INT,
 latest_dispensing_encounter_id	INT(11),
-latest_dispensing_date			DATETIME,
+latest_dispensing_date		DATETIME,
 latest_expected_dispensing_date	DATETIME,
-dispensing_days_late			INT,
+dispensing_days_late		INT,
 latest_hiv_viral_load_obs_group	INT(11),
-latest_hiv_viral_load_date		DATETIME,
-latest_hiv_viral_load_coded		VARCHAR(255),
-latest_hiv_viral_load			INT,
-latest_arv_regimen_order_id		INT(11),
-latest_arc_regimen_encounter_id	INT(11),
-latest_arv_regimen_date			DATETIME,
-latest_arv_regimen_line			VARCHAR(255),
-latest_arv_dispensed_id			INT(11),
-latest_arv_dispensed_date		DATETIME,
-latest_arv_dispensed_line		VARCHAR(255),
- PRIMARY KEY (monthly_reporting_id)
+latest_hiv_viral_load_date	DATETIME,
+latest_hiv_viral_load_coded	VARCHAR(255),
+latest_hiv_viral_load		INT,
+latest_arv_regimen_encounter_id	INT(11),
+latest_arv_regimen_date		DATETIME,
+latest_arv_regimen_line		VARCHAR(255),
+latest_arv_dispensed_id		INT(11),
+latest_arv_dispensed_date	DATETIME,
+latest_arv_dispensed_line	VARCHAR(255),
+PRIMARY KEY (monthly_reporting_id)
 );
 
 create index eom_patient on temp_eom_appts(patient_id);
@@ -110,25 +109,24 @@ set latest_hiv_viral_load	= obs_from_group_id_value_numeric(latest_hiv_viral_loa
 -- this is used to update the main temp table
 drop temporary table if exists temp_last_arv_order_id;
 create temporary table temp_last_arv_order_id 
-	select t.monthly_reporting_id , o.order_id, o.date_activated, o.encounter_id  
+	select t.monthly_reporting_id, o.date_activated, o.encounter_id  
 	from temp_eom_appts t 
 	inner join 
-		(select o2.order_id, o2.patient_id ,  o2.date_activated, o2.encounter_id  
+		(select o2.patient_id ,  o2.date_activated, o2.encounter_id  
 		from orders o2 
 		where o2.voided = 0
-		and o2.order_reason = concept_from_mapping('CIEL','138405')
-		order by if(o2.date_stopped is null,0,1) asc, o2.date_activated desc) o on o.patient_id = t.patient_id and o.date_activated < t.reporting_date  
+		and o2.order_reason = concept_from_mapping('CIEL','138405') -- HIV order reason
+		order by if(o2.date_stopped is null,0,1) asc, ifnull(o2.scheduled_date, o2.date_activated) desc) o on o.patient_id = t.patient_id and o.date_activated < t.reporting_date  
 	group by t.monthly_reporting_id 
 ;
 
 update temp_eom_appts t 
 inner join temp_last_arv_order_id tlao on tlao.monthly_reporting_id = t.monthly_reporting_id 
-set latest_arv_regimen_order_id = tlao.order_id,
-	latest_arv_regimen_date = tlao.date_activated,
-	latest_arc_regimen_encounter_id = tlao.encounter_id ;
+set latest_arv_regimen_date = tlao.date_activated,
+	latest_arv_regimen_encounter_id = tlao.encounter_id ;
 
 update temp_eom_appts t 
-set latest_arv_regimen_line = obs_value_coded_list(t.latest_arc_regimen_encounter_id, 'PIH','13115',@locale );
+set latest_arv_regimen_line = obs_value_coded_list(t.latest_arv_regimen_encounter_id, 'PIH','13115',@locale );
 
 -- latest arv dispensing
 -- a temp table is loaded with the most recent HIV dispensing encounter information for each row
@@ -140,12 +138,12 @@ create temporary table temp_last_arv_dispensing_id
 	inner join 
 		(select e2.patient_id, e2.encounter_id, e2.encounter_datetime 
 		from encounter e2
-		inner join obs o on o.voided = 0 and o.encounter_id = e2.encounter_id and  o.concept_id =  concept_from_mapping('PIH','1535')
-			and o.value_coded in (concept_from_mapping('PIH','3013'),concept_from_mapping('PIH','2848'),concept_from_mapping('PIH','13960'))
+		inner join obs o on o.voided = 0 and o.encounter_id = e2.encounter_id and  o.concept_id =  concept_from_mapping('PIH','1535') -- medication name
+			and o.value_coded in (concept_from_mapping('PIH','3013'),concept_from_mapping('PIH','2848'),concept_from_mapping('PIH','13960')) -- ARV1, ARV2 and ARV3
 		where e2.voided = 0
 		and e2.encounter_type = @hiv_Dispensing_id 
 		order by e2.encounter_datetime desc, e2.encounter_id desc) e on e.patient_id  = t.patient_id and e.encounter_datetime <= t.reporting_date 
-	group by t.monthly_reporting_id ;		
+	group by t.monthly_reporting_id;		
 		
 update temp_eom_appts t 
 inner join temp_last_arv_dispensing_id tlad on tlad.monthly_reporting_id = t.monthly_reporting_id 
