5ded8c9b81ed15e921dc6ae45e7c1e7b36915ea4
dmdesimone <ddesimone@pih.org>
2022-04-19 16:28:58 +0200

    clean up bad merge artifacts (uhm-6397)
    clean up bad merge artifacts

--- jobs/sql/extractions/hiv_tests.sql
+++ jobs/sql/extractions/hiv_tests.sql
@@ -166,11 +166,6 @@ create index temp_hiv_lab_index_asc_test_type on temp_hiv_lab_index_asc(test_typ
 create index temp_hiv_lab_index_desc_encounter_id on temp_hiv_lab_index_desc(encounter_id);
 create index temp_hiv_lab_index_desc_test_type on temp_hiv_lab_index_desc(test_type);	
 
-create index temp_hiv_lab_index_asc_encounter_id on temp_hiv_lab_index_asc(encounter_id);
-create index temp_hiv_lab_index_asc_test_type on temp_hiv_lab_index_asc(test_type);
-create index temp_hiv_lab_index_desc_encounter_id on temp_hiv_lab_index_desc(encounter_id);
-create index temp_hiv_lab_index_desc_test_type on temp_hiv_lab_index_desc(test_type);
-
 UPDATE temp_lab_tests_final tbf JOIN temp_hiv_lab_index_asc tbia ON tbf.encounter_id = tbia.encounter_id AND tbf.test_type = tbia.test_type
 SET tbf.index_asc = tbia.index_asc;
 
