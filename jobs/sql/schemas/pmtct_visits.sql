e5f1d54f1b890180e98b87e7305fcb8766f13fc4
dmdesimone <ddesimone@pih.org>
2022-06-03 22:06:31 -0400

    add expected_delivery_date
    add expected_delivery_date

--- jobs/sql/schemas/pmtct_visits.sql
+++ jobs/sql/schemas/pmtct_visits.sql
@@ -1,15 +1,16 @@
 CREATE TABLE pmtct_visits
 (
-    visit_id             INT,
-    encounter_id         VARCHAR(25),
-    emr_id               VARCHAR(25),
-    visit_date           DATE,
-    health_facility      VARCHAR(100),
-    date_entered         DATETIME,
-    user_entered         VARCHAR(50),
-    hiv_test_date        DATE,
-    tb_screening_date    DATE,
-    has_provided_contact BIT,
-    index_asc            INT,
-    index_desc           INT
+    visit_id                INT,
+    encounter_id            VARCHAR(25),
+    emr_id                  VARCHAR(25),
+    visit_date              DATE,
+    health_facility         VARCHAR(100),
+    date_entered            DATETIME,
+    user_entered            VARCHAR(50),
+    hiv_test_date           DATE,
+    expected_delivery_date  DATE
+    tb_screening_date       DATE,
+    has_provided_contact    BIT,
+    index_asc               INT,
+    index_desc              INT
 );
