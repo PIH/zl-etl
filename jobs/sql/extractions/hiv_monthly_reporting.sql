bfff5eae4144d7c43a5ae97f1831c130f4ccc0d8
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-06-09 13:56:51 +0200

    changed in script

--- jobs/sql/extractions/hiv_monthly_reporting.sql
+++ jobs/sql/extractions/hiv_monthly_reporting.sql
@@ -75,7 +75,7 @@ FROM hiv_patient_program hpp
 ) x
 WHERE CASE WHEN next_date_enrolled=date_completed THEN 0 ELSE 1 END=1;
 
-INSERT INTO temp_eom_appts (emr_id, date_enrolled, date_completed,reporting_date )
+INSERT INTO #temp_eom_appts (emr_id, date_enrolled, date_completed,reporting_date )
 SELECT DISTINCT emr_id AS patient_id, date_enrolled ,date_completed, dd.LastDateofMonth reporting_date
 FROM hiv_patient_modified hpp
 inner join Dim_Date dd  
@@ -85,9 +85,9 @@ inner join Dim_Date dd
 
 
 -- ############################### HIV Visit Data ##################################################################
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_hiv_visit_date = x.visit_date
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_hiv_visit_date = x.visit_date
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN 
 (
 	SELECT emr_id,reporting_date,max(visit_date) visit_date FROM all_reporting_visits
@@ -95,11 +95,11 @@ LEFT OUTER JOIN
 ) x
 ON t1.emr_id =  x.emr_id AND t1.reporting_date=x.reporting_date;
 
-UPDATE temp_eom_appts
+UPDATE t1
 SET 
-temp_eom_appts.latest_expected_hiv_visit_date =av.next_visit_date,
-temp_eom_appts.hiv_visit_days_late=DATEDIFF(DAY,isnull(av.next_visit_date,isnull(t1.latest_hiv_visit_date,t1.date_enrolled)),t1.reporting_date)
-FROM  temp_eom_appts t1 
+t1.latest_expected_hiv_visit_date =av.next_visit_date,
+t1.hiv_visit_days_late=DATEDIFF(DAY,isnull(av.next_visit_date,isnull(t1.latest_hiv_visit_date,t1.date_enrolled)),t1.reporting_date)
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN all_reporting_visits av
 ON t1.emr_id =  av.emr_id 
 AND t1.reporting_date=av.reporting_date
@@ -107,9 +107,9 @@ AND t1.latest_hiv_visit_date=av.visit_date;
 
 -- ############################### HIV Despensing Data ##################################################################
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_dispensing_date = x.dispense_date
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_dispensing_date = x.dispense_date
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN 
 (
 	SELECT emr_id,reporting_date,max(dispense_date)  dispense_date FROM all_reporting_dispense
@@ -117,11 +117,11 @@ LEFT OUTER JOIN
 ) x
 ON t1.emr_id =  x.emr_id AND t1.reporting_date=x.reporting_date;
 
-UPDATE temp_eom_appts
+UPDATE t1
 SET 
-temp_eom_appts.latest_expected_dispensing_date=ad.next_dispense_date,
-temp_eom_appts.dispensing_days_late=DATEDIFF(DAY,isnull(ad.next_dispense_date,isnull(t1.latest_dispensing_date,t1.date_enrolled)),t1.reporting_date)
-FROM  temp_eom_appts t1 
+t1.latest_expected_dispensing_date=ad.next_dispense_date,
+t1.dispensing_days_late=DATEDIFF(DAY,isnull(ad.next_dispense_date,isnull(t1.latest_dispensing_date,t1.date_enrolled)),t1.reporting_date)
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN all_reporting_dispense ad
 ON t1.emr_id =  ad.emr_id 
 AND t1.reporting_date=ad.reporting_date
@@ -129,9 +129,9 @@ AND t1.latest_dispensing_date=ad.dispense_date;
 
 -- ############################### HIV Viral Data ##################################################################
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_hiv_viral_load_date = x.vl_sample_taken_date
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_hiv_viral_load_date = x.vl_sample_taken_date
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN 
 (
 	SELECT emr_id,reporting_date,max(vl_sample_taken_date)  vl_sample_taken_date FROM all_reporting_viral
@@ -140,10 +140,10 @@ LEFT OUTER JOIN
 ON t1.emr_id =  x.emr_id AND t1.reporting_date=x.reporting_date;
 
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_hiv_viral_load_coded = avl.vl_coded_results,
-temp_eom_appts.latest_hiv_viral_load=avl.viral_load
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_hiv_viral_load_coded = avl.vl_coded_results,
+t1.latest_hiv_viral_load=avl.viral_load
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN all_reporting_viral avl
 ON t1.emr_id =  avl.emr_id 
 AND t1.reporting_date=avl.reporting_date
@@ -151,9 +151,9 @@ AND t1.latest_hiv_viral_load_date=avl.vl_sample_taken_date;
 
 -- ############################### HIV Regimens ##################################################################
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_arv_regimen_date = x.encounter_datetime
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_arv_regimen_date = x.encounter_datetime
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN 
 (
 	SELECT emr_id,reporting_date,max(encounter_datetime)  encounter_datetime 
@@ -163,9 +163,9 @@ LEFT OUTER JOIN
 ON t1.emr_id =  x.emr_id AND t1.reporting_date=x.reporting_date;
 
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_arv_regimen_line = r.art_treatment_line
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_arv_regimen_line = r.art_treatment_line
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN all_reporting_reg r
 ON t1.emr_id =  r.emr_id 
 AND t1.reporting_date=r.reporting_date
@@ -173,9 +173,9 @@ AND t1.latest_arv_regimen_date=r.encounter_datetime;
 
 -- ############################### HIV Despense ARV ##################################################################
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_arv_dispensed_date = x.dispense_date
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_arv_dispensed_date = x.dispense_date
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN 
 (
 	SELECT emr_id,reporting_date,max(dispense_date)  dispense_date 
@@ -184,9 +184,9 @@ LEFT OUTER JOIN
 ) x
 ON t1.emr_id =  x.emr_id AND t1.reporting_date=x.reporting_date;
 
-UPDATE temp_eom_appts
-SET temp_eom_appts.latest_arv_dispensed_line = ad.current_art_treatment_line
-FROM  temp_eom_appts t1 
+UPDATE t1
+SET t1.latest_arv_dispensed_line = ad.current_art_treatment_line
+FROM  #temp_eom_appts t1 
 LEFT OUTER JOIN all_reporting_dispense_arv ad
 ON t1.emr_id =  ad.emr_id 
 AND t1.reporting_date=ad.reporting_date
@@ -213,4 +213,4 @@ latest_arv_regimen_line,
 latest_arv_dispensed_id,
 latest_arv_dispensed_date,
 latest_arv_dispensed_line
-FROM temp_eom_appts;
+FROM #temp_eom_appts;
