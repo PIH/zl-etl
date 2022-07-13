f3457d5270f1d31540966074650fbd71d6da6c27
dmdesimone <ddesimone@pih.org>
2022-07-13 14:24:38 -0400

    update referral fields
    update referral fields

--- jobs/sql/extractions/hiv_visit.sql
+++ jobs/sql/extractions/hiv_visit.sql
@@ -5,35 +5,37 @@ SET @hiv_intake = (SELECT encounter_type_id FROM encounter_type WHERE uuid = 'c3
 SET @hiv_followup = (SELECT encounter_type_id FROM encounter_type WHERE uuid = 'c31d3312-40c4-11e7-a919-92ebcb67fe33');
 	
 DROP TEMPORARY TABLE IF EXISTS temp_hiv_visit;
+
 CREATE TEMPORARY TABLE temp_hiv_visit
 (
-encounter_id		INT,
-patient_id		INT,
-emr_id			VARCHAR(25),
-hivemr_v1		VARCHAR(25),	
-encounter_type 		VARCHAR(255),
-date_entered    	DATETIME,
-user_entered    	VARCHAR(50),
-chw			VARCHAR(255),
-pregnant_obs_id		INT(11),
-pregnant		BIT,
-visit_date		DATE,
-referral_transfer	VARCHAR(255),
-internal_external	VARCHAR(255),
-rt_location_id		TEXT,
-referral_transfer_location		VARCHAR(255),
-referred_by_womens_health_obs_id	INT(11),
-referred_by_womens_health 		BIT,
-referral_transfer_pepfar_partner_obs_id	INT(11),
-referral_transfer_pepfar_partner 	BIT,
-reason_not_on_ARV	VARCHAR(255),
-next_visit_date		DATE,
-encounter_location_id	INT(11),
-visit_location		VARCHAR(255),
-index_asc		INT,
-index_desc		INT
+encounter_id								INT,
+patient_id									INT,
+emr_id										VARCHAR(25),
+hivemr_v1									VARCHAR(25),	
+encounter_type								VARCHAR(255),
+date_entered								DATETIME,
+user_entered								VARCHAR(50),
+chw											VARCHAR(255),
+pregnant									BIT,
+visit_date									DATE,
+rt_in_obs_group_id							INT(11),
+referral_transfer_in						VARCHAR(255),
+internal_external_in						VARCHAR(255),
+referral_transfer_location_in				VARCHAR(255),
+referred_by_womens_health_in				BIT,
+referral_transfer_pepfar_partner_in			BIT,
+rt_out_obs_group_id							INT(11),
+referral_transfer_out						VARCHAR(255),
+internal_external_out						VARCHAR(255),
+referral_transfer_location_out				VARCHAR(255),
+referral_transfer_pepfar_partner_out		BIT,
+reason_not_on_ARV							VARCHAR(255),
+next_visit_date								DATE,
+encounter_location_id						INT(11),
+visit_location								VARCHAR(255),
+index_asc									INT,
+index_desc									INT
 );
-
 
 INSERT INTO temp_hiv_visit(patient_id, encounter_id, emr_id, visit_date,encounter_type, date_entered, user_entered, encounter_location_id)
 SELECT patient_id, encounter_id, ZLEMR(patient_id),  DATE(encounter_datetime), encounter_type_name(encounter_id), date_created, username(creator), location_id  FROM encounter  WHERE voided = 0 AND encounter_type IN (@hiv_intake, @hiv_followup)
@@ -42,9 +44,8 @@ SELECT patient_id, encounter_id, ZLEMR(patient_id),  DATE(encounter_datetime), e
 CREATE INDEX temp_hiv_visit_pid ON temp_hiv_visit (patient_id);
 CREATE INDEX temp_hiv_visit_eid ON temp_hiv_visit (encounter_id);
 	
-DELETE FROM temp_hiv_visit
-WHERE
-    patient_id IN (SELECT
+DELETE FROM temp_hiv_visit 
+WHERE patient_id IN (SELECT
         a.person_id
     FROM
         person_attribute a
@@ -65,29 +66,96 @@ inner join temp_hiv_visit t on t.encounter_id = o.encounter_id
 where o.voided = 0;
 
 create index temp_obs_ci1 on temp_obs(encounter_id,concept_id);
-create index temp_obs_ci2 on temp_obs(date_created, obs_id);
-create index temp_obs_oi on temp_obs(obs_id);
+create index temp_obs_ci3 on temp_obs(obs_group_id,concept_id);
 
-update temp_hiv_visit t set chw = obs_value_text_from_temp(t.encounter_id, 'PIH','11631');	
+update temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','11631') and o.voided = 0
+set	chw = o.value_text;
 
-UPDATE temp_hiv_visit t SET pregnant_obs_id = obs_id_from_temp(t.encounter_id, 'PIH', 'PREGNANCY STATUS',0);
-UPDATE temp_hiv_visit t SET pregnant = value_coded_as_boolean_from_temp(pregnant_obs_id);
+update temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','PREGNANCY STATUS') and o.voided = 0
+set	pregnant =	
+	 CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END;
 
-UPDATE temp_hiv_visit t SET next_visit_date = obs_value_datetime_from_temp(t.encounter_id,'PIH','RETURN VISIT DATE');
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','RETURN VISIT DATE')	and o.voided = 0
+set	next_visit_date = o.value_datetime;
 
-UPDATE temp_hiv_visit t SET referral_transfer = obs_value_coded_list_from_temp(t.encounter_id,'PIH','13712',@locale);
-UPDATE temp_hiv_visit t SET internal_external = obs_value_coded_list_from_temp(t.encounter_id,'PIH','6401',@locale);
-UPDATE temp_hiv_visit t SET rt_location_id = obs_value_text_from_temp(t.encounter_id,'PIH','8621');
-UPDATE temp_hiv_visit t SET referral_transfer_location = 
-CASE 
-	when internal_external = concept_name(concept_from_mapping('PIH',8855),@locale) then location_name(rt_location_id)
-	else rt_location_id
-END;
+-- transfers, referrals IN:
+UPDATE 	temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','13169') and o.voided = 0
+set	rt_in_obs_group_id = o.obs_id;
 
-UPDATE temp_hiv_visit t SET referred_by_womens_health_obs_id = obs_id_from_temp(t.encounter_id, 'PIH', '13679',0);
-UPDATE temp_hiv_visit t SET referred_by_womens_health = value_coded_as_boolean_from_temp(referred_by_womens_health_obs_id);
-UPDATE temp_hiv_visit t SET referral_transfer_pepfar_partner_obs_id = obs_id_from_temp(t.encounter_id, 'PIH', '13168',0);
-UPDATE temp_hiv_visit t SET referral_transfer_pepfar_partner = value_coded_as_boolean_from_temp(referral_transfer_pepfar_partner_obs_id);
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_in_obs_group_id and o.concept_id = concept_from_mapping('PIH','13712') and o.voided = 0
+set	referral_transfer_in = concept_name(o.value_coded,@locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_in_obs_group_id and o.concept_id = concept_from_mapping('PIH','6401') and o.voided = 0
+set	internal_external_in = concept_name(o.value_coded, @locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_in_obs_group_id and o.concept_id = concept_from_mapping('PIH','8621') and o.voided = 0
+set referral_transfer_location_in = location_name(o.value_text)
+where internal_external_in = concept_name(concept_from_mapping('PIH','8855'),@locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_in_obs_group_id and o.concept_id = concept_from_mapping('PIH','11483') and o.voided = 0
+set referral_transfer_location_in = o.value_text
+where internal_external_in <> concept_name(concept_from_mapping('PIH','8855'),@locale);
+
+update temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','13679')	and o.voided = 0
+set	referred_by_womens_health_in =	
+	 CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END;
+
+update temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','13168')	and o.voided = 0
+set referral_transfer_pepfar_partner_in =	
+	 CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END;
+
+-- transfers, referrals OUT:
+UPDATE 	temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH','13170') and o.voided = 0
+set rt_out_obs_group_id = o.obs_id;
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_out_obs_group_id and o.concept_id = concept_from_mapping('PIH','13712') and o.voided = 0
+set	referral_transfer_out = concept_name(o.value_coded,	@locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_out_obs_group_id and o.concept_id = concept_from_mapping('PIH','8854') and o.voided = 0
+set	internal_external_out = concept_name(o.value_coded,	@locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_out_obs_group_id	and o.concept_id = concept_from_mapping('PIH','8621') and o.voided = 0
+set referral_transfer_location_out = location_name(o.value_text)
+where
+	internal_external_out = concept_name(concept_from_mapping('PIH','8855'),@locale);
+
+UPDATE temp_hiv_visit t
+inner join temp_obs o on o.obs_group_id = t.rt_out_obs_group_id and o.concept_id = concept_from_mapping('PIH','11483') and o.voided = 0
+set	referral_transfer_location_out = o.value_text
+where
+	internal_external_out <> concept_name(concept_from_mapping('PIH','8855'),@locale);
+
+update temp_hiv_visit t
+inner join temp_obs o on o.encounter_id = t.encounter_id and o.concept_id = concept_from_mapping('PIH',	'13168') and o.voided = 0
+set	referral_transfer_pepfar_partner_out =	
+	 CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END;
+
 UPDATE temp_hiv_visit t 
 	inner join temp_obs tobs on tobs.encounter_id = t.encounter_id and concept_id = concept_from_mapping('PIH','11337') 
 	and tobs.value_coded in 
@@ -152,24 +220,29 @@ update temp_hiv_visit t
 inner join temp_visit_index_desc tvid on tvid.encounter_id = t.encounter_id
 set t.index_desc = tvid.index_desc;
 
-SELECT 
-concat(@partition,'-',encounter_id),
-emr_id,
-hivemr_v1,	
-encounter_type,
-date_entered,
-user_entered,
-chw,
-pregnant,
-referral_transfer,
-internal_external,
-referral_transfer_location,
-referred_by_womens_health,
-referral_transfer_pepfar_partner,
-reason_not_on_ARV,
-visit_date,
-next_visit_date,
-visit_location,
-index_asc,
-index_desc
-FROM temp_hiv_visit;
+SELECT
+	concat(@partition, '-', encounter_id),
+	emr_id,
+	hivemr_v1,
+	encounter_type,
+	date_entered,
+	user_entered,
+	chw,
+	pregnant,
+	referral_transfer_in,
+	internal_external_in,
+	referral_transfer_location_in,
+	referred_by_womens_health_in,
+	referral_transfer_pepfar_partner_in,
+	referral_transfer_out,
+	internal_external_out,
+	referral_transfer_location_out,
+	referral_transfer_pepfar_partner_out,
+	reason_not_on_ARV,
+	visit_date,
+	next_visit_date,
+	visit_location,
+	index_asc,
+	index_desc
+FROM
+	temp_hiv_visit t;
