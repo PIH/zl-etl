8e93d5bf35c2fc8be366602d7ca912348536b204
dmdesimone <ddesimone@pih.org>
2022-08-12 08:01:58 -0400

    fix datatype of tb_lab_results_id

--- jobs/sql/schemas/tb_lab_results.sql
+++ jobs/sql/schemas/tb_lab_results.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tb_lab_results
 (
-    tb_lab_results_id           INT,
+    tb_lab_results_id           VARCHAR(50),
     emr_id                      VARCHAR(50),
     dossier_id                  VARCHAR(50),
     encounter_id                VARCHAR(25),
