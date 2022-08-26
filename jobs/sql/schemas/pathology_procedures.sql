7b3a64689ee68d3e0227b76f88380ea8174a94a1
dmdesimone <ddesimone@pih.org>
2022-08-26 16:49:09 -0400

    Create pathology_procedures.sql

---
+++ jobs/sql/schemas/pathology_procedures.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pathology_procedures
+(
+patient_id					INT,
+emr_id						VARCHAR(50),
+encounter_id				VARCHAR(50),
+order_id					VARCHAR(50),
+order_number 				VARCHAR(50),
+coded_procedure				VARCHAR(255),
+noncoded_procedure			VARCHAR(255)
+);
