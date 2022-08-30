9850225605ff10a2890f4e5c5c0c90f15c3fcb9e
dmdesimone <ddesimone@pih.org>
2022-08-30 10:05:57 -0400

    reorder schema columns

--- jobs/sql/schemas/pathology_procedures.sql
+++ jobs/sql/schemas/pathology_procedures.sql
@@ -1,9 +1,9 @@
 CREATE TABLE pathology_procedures
 (
-emr_id						VARCHAR(50),
-encounter_id				VARCHAR(50),
-order_id					VARCHAR(50),
-order_number 				VARCHAR(50),
-coded_procedure				VARCHAR(255),
-noncoded_procedure			VARCHAR(255)
+emr_id						  VARCHAR(50),
+order_id					  VARCHAR(50),
+order_number 			  VARCHAR(50),  
+encounter_id			  VARCHAR(50),
+coded_procedure		  VARCHAR(255),
+noncoded_procedure  VARCHAR(255)
 );
