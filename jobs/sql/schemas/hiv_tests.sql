b7b3ec62e7ca06ff1686b68c2805cc0ba684546b
dmdesimone <ddesimone@pih.org>
2022-09-22 16:17:08 -0400

    add encounter_type column

--- jobs/sql/schemas/hiv_tests.sql
+++ jobs/sql/schemas/hiv_tests.sql
@@ -1,11 +1,12 @@
 CREATE TABLE hiv_tests (
-    emr_id                      VARCHAR(25),
-    hivemr_v1_id                VARCHAR(25),
+    emr_id                      VARCHAR(50),
+    hivemr_v1_id                VARCHAR(50),
     encounter_id                VARCHAR(25),
+    encounter_type              VARCHAR(50),
     specimen_collection_date    DATE,
     result_date                 DATE,
-    test_type                   VARCHAR(100),
-    test_result                 VARCHAR(100),
+    test_type                   VARCHAR(255),
+    test_result                 VARCHAR(255),
     index_asc                   INT,
     index_desc                  INT
 );
