9a5b202cb1a9c2ad2415a03379a55088b18e71cd
dmdesimone <ddesimone@pih.org>
2022-10-13 14:32:52 -0400

    improve performance, remove index_asc/desc

--- jobs/sql/extractions/all_encounters.sql
+++ jobs/sql/extractions/all_encounters.sql
@@ -7,24 +7,19 @@ create temporary table temp_all_encounters
 (
 encounter_id        int,
 encounter_datetime	datetime, 
-encounter_type      int,
-location_id         int,
 patient_id          int, 
 visit_id            int, 
-creator             int(11),
 user_entered        varchar(255),
 encounter_location  varchar(255),
 encounter_type_name varchar(50),
 entered_datetime    datetime,
 emr_id              varchar(15),
-next_appt_date      date,
-index_asc           int,
-index_desc          int		
+next_appt_date      date
 );
 
 Insert into temp_all_encounters (
-encounter_id, encounter_datetime, encounter_type, location_id, patient_id, visit_id, creator, entered_datetime)
-select encounter_id, encounter_datetime, encounter_type, location_id, patient_id, visit_id, creator, date_created 
+encounter_id, encounter_datetime, encounter_type_name, encounter_location, patient_id, visit_id, user_entered, entered_datetime)
+select encounter_id, encounter_datetime, encounter_type_name_from_id(encounter_type), location_name(location_id), patient_id, visit_id, person_name(creator), date_created 
 from encounter 
 where voided = 0
 ;
@@ -32,9 +27,7 @@ where voided = 0
 -- index
 CREATE INDEX temp_all_encounters_patientId ON temp_all_encounters(patient_id);
 CREATE INDEX temp_all_encounters_encounterId ON temp_all_encounters(encounter_id);
-CREATE INDEX temp_all_encounters_encounterType ON temp_all_encounters(encounter_type);
-CREATE INDEX temp_all_encounters_encounterLoc ON temp_all_encounters(location_id);
-CREATE INDEX temp_all_encountersCreator ON temp_all_encounters(creator);
+-- CREATE INDEX temp_all_encountersCreator ON temp_all_encounters(creator);
 
 -- delete encounters for voided patients
 delete t 
@@ -42,16 +35,6 @@ from temp_all_encounters t
 inner join patient p on p.patient_id = t.patient_id
 where p.voided = 1;
 
--- location name
-update temp_all_encounters t set encounter_location = location_name(t.location_id);  
-
---  encounter type
-update temp_all_encounters t set encounter_type_name = encounter_type_name_from_id(t.encounter_type);
-
--- user entered
-update temp_all_encounters t 
-inner join users u on t.creator  = u.user_id 
-set user_entered =  person_name(u.person_id) ;
 
 -- emr_id
 -- unique patients loaded to temp table before looking up emr_id and joining back in to the temp_all_encounters table
@@ -87,60 +70,7 @@ update temp_all_encounters te
 inner join temp_next_appt_obs tvo on tvo.encounter_id = te.encounter_id
 set te.next_appt_date = tvo.next_appt_date;
 
--- indexes
-drop temporary table if exists temp_all_encounters_index_asc;
-create temporary table temp_all_encounters_index_asc
-(
-	SELECT  
-           patient_id,
-           encounter_id,
-           encounter_datetime,
-		   index_asc
-FROM (SELECT  
-             @r:= IF(@u = patient_id, @r + 1,1) index_asc,
-             patient_id,
-             encounter_id,
-             encounter_datetime,
-			 @u:= patient_id
-            FROM temp_all_encounters,
-                    (SELECT @r:= 1) AS r,
-                    (SELECT @u:= 0) AS u
-            ORDER BY patient_id, encounter_id ASC
-            ) index_ascending );
-
-CREATE INDEX teia_ei ON temp_all_encounters_index_asc (encounter_id);   
-
-update temp_all_encounters t
-inner join temp_all_encounters_index_asc teia on teia.encounter_id = t.encounter_id
-set t.index_asc = teia.index_asc;
-
-drop temporary table if exists temp_all_encounters_index_desc;
-create temporary table temp_all_encounters_index_desc
-(
-			SELECT  
-           patient_id,
-           encounter_id,
-           encounter_datetime,
-  		   index_asc,
-           index_desc
-FROM (SELECT  
-             @r:= IF(@u = patient_id, @r + 1,1) index_desc,
-             patient_id,
-             encounter_id,
-             encounter_datetime,
-             index_asc,
-			 @u:= patient_id
-            FROM temp_all_encounters,
-                    (SELECT @r:= 1) AS r,
-                    (SELECT @u:= 0) AS u
-            ORDER BY patient_id, encounter_id DESC
-        ) index_descending );
-
-CREATE INDEX teid_e1 ON temp_all_encounters_index_desc (encounter_id);   
-
-update temp_all_encounters t
-inner join temp_all_encounters_index_desc teid on teid.encounter_id = t.encounter_id
-set t.index_desc = teid.index_desc;
+-- NOTE:  the index_asc, index_desc will be calculated in a later job in the pipeline
 
 -- final query
 select 
@@ -153,7 +83,7 @@ select
    entered_datetime,
    user_entered,
    next_appt_date,
-   index_asc,
-   index_desc
+   null, -- index_asc 
+   null -- index desc
 from temp_all_encounters t
 ORDER BY t.patient_id, t.encounter_id;
