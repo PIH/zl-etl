1f72e4a1d3e563543c754346d5d72d6acf4f6ec4
Michael Seaton <mseaton@pih.org>
2022-10-25 14:37:03 -0400

    UHM-6732 - Fix incorrect schedule placement.

--- jobs/sql/schemas/user_logins.sql
+++ jobs/sql/schemas/user_logins.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user_logins
 (
-    session_id               char(36),
-    username                 varchar(50),
-    date_logged_in           datetime,
-    date_logged_out          datetime,
-    date_session_destroyed   datetime,
-    session_duration_minutes int
+    session_id              char(36),
+    username                varchar(50),
+    date_logged_in          datetime,
+    date_logged_out         datetime,
+    date_expired            datetime,
+    active_duration_minutes int
 );
