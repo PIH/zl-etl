d502de6a526a6f57b47c97596ba917c7357108a2
dmdesimone <ddesimone@pih.org>
2022-11-01 16:05:03 -0400

    add voided column

--- jobs/sql/schemas/all_encounters_staging.sql
+++ jobs/sql/schemas/all_encounters_staging.sql
@@ -8,5 +8,6 @@ encounter_location  varchar(255),
 encounter_datetime  datetime, 
 entered_datetime    datetime,
 user_entered        varchar(255),
-next_appt_date      date	
+next_appt_date      date,
+voided              bit
 );
