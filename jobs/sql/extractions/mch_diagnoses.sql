7de4b2fdc3627d807e01e58b24a62d12bbad9844
James Mbabazi <jmbabazi@pih.org>
2023-02-22 11:17:31 -0500

    mch_diagnoses using vars (@yes, @no)

--- jobs/sql/extractions/mch_diagnoses.sql
+++ jobs/sql/extractions/mch_diagnoses.sql
@@ -1,6 +1,8 @@
 set @locale =   if(@startDate is null, 'en', global_property_value('default_locale', 'en'));
 set @mch_encounter = (select encounter_type_id from encounter_type where uuid = '00e5ebb2-90ec-11e8-9eb6-529269fb1459');
 set sql_safe_updates = 0;
+set @yes = 1;
+set @no = 0;
 
 drop temporary table if exists temp_mch_diagnoses;
 create temporary table temp_mch_diagnoses
@@ -87,8 +89,8 @@ update temp_mch_diagnoses tm set visit_id = (select visit_id from encounter e wh
 -- Categories
 -- Abortion
 
-update temp_mch_diagnoses tm set abortion = if(diagnosis_concept in (concept_from_mapping('CIEL', '120295'), concept_from_mapping('PIH', '7697'), concept_from_mapping('CIEL', '162206')), 1, 0);
-update temp_mch_diagnoses tm set abortion_with_sepsis = if(diagnosis_concept = concept_from_mapping('CIEL', '150746'), 1, 0);
+update temp_mch_diagnoses tm set abortion = if(diagnosis_concept in (concept_from_mapping('CIEL', '120295'), concept_from_mapping('PIH', '7697'), concept_from_mapping('CIEL', '162206')), @yes, @no);
+update temp_mch_diagnoses tm set abortion_with_sepsis = if(diagnosis_concept = concept_from_mapping('CIEL', '150746'), @yes, @no);
 update temp_mch_diagnoses tm set anemia = if(diagnosis_concept in (
 concept_from_mapping('CIEL', '149566'), 
 concept_from_mapping('PIH', '3703'), 
@@ -101,15 +103,15 @@ concept_from_mapping('PIH', '9089'),
 concept_from_mapping('PIH', '3'),
 concept_from_mapping('PIH', 'ANEMIA OF PREGNANCY'),
 concept_from_mapping('PIH', '9431')
-), 1, 0);
+), @yes, @no);
 
-update temp_mch_diagnoses tm set cervical_cancer = if(diagnosis_concept = concept_from_mapping('CIEL', '116023'), 1, 0);
+update temp_mch_diagnoses tm set cervical_cancer = if(diagnosis_concept = concept_from_mapping('CIEL', '116023'), @yes, @no);
 
 update temp_mch_diagnoses tm set cervical_laceration = if(diagnosis_concept = 
-concept_from_mapping('CIEL', '145804'), 1, 0);
+concept_from_mapping('CIEL', '145804'), @yes, @no);
 
 update temp_mch_diagnoses tm set complete_abortion = if(diagnosis_concept = 
-concept_from_mapping('CIEL', '120295'), 1, 0);
+concept_from_mapping('CIEL', '120295'), @yes, @no);
 
 update temp_mch_diagnoses tm set diabetes = if(diagnosis_concept in (
 concept_from_mapping('PIH', '7961'), 
@@ -130,22 +132,22 @@ concept_from_mapping('PIH', '3720'),
 concept_from_mapping('PIH', '225'),
 concept_from_mapping('PIH', '7961'),
 concept_from_mapping('PIH', '6693')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set dystocia = if(diagnosis_concept in (
 concept_from_mapping('PIH', '8017'), 
 concept_from_mapping('PIH', '8031')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set eclampsia = if(diagnosis_concept = 
-concept_from_mapping('PIH', '7696'), 1, 0);
+concept_from_mapping('PIH', '7696'), @yes, @no);
 
 update temp_mch_diagnoses tm set hemorrhage = if(diagnosis_concept in
 (
 concept_from_mapping('PIH', '228'),
 concept_from_mapping('PIH', '7235'),
 concept_from_mapping('PIH', '7102')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set hypertension = if(diagnosis_concept in
 (
@@ -155,7 +157,7 @@ concept_from_mapping('PIH', '12629'),
 concept_from_mapping('PIH', '12634'),
 concept_from_mapping('PIH', '14307'),
 concept_from_mapping('PIH', '9058')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set incomplete_abortion = if(diagnosis_concept in
 (
@@ -164,20 +166,20 @@ concept_from_mapping('PIH', '9727'),
 concept_from_mapping('PIH', '905'),
 concept_from_mapping('PIH', '9335'),
 concept_from_mapping('PIH', '8319')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set induced_abortion = if(diagnosis_concept = 
-concept_from_mapping('PIH', '9727'), 1, 0);
+concept_from_mapping('PIH', '9727'), @yes, @no);
 
 update temp_mch_diagnoses tm set postpartum_hemorrhage = if(diagnosis_concept =
-concept_from_mapping('PIH', '49'), 1, 0);
+concept_from_mapping('PIH', '49'), @yes, @no);
 
 update temp_mch_diagnoses tm set laceration_of_perineum = if(diagnosis_concept in
 (
 concept_from_mapping('CIEL', '123620'),
 concept_from_mapping('PIH', '12372'),
 concept_from_mapping('PIH', '7230')
-), 1, 0);
+), @yes, @no);
 
 
 update temp_mch_diagnoses tm set malaria = if(diagnosis_concept in
@@ -187,10 +189,10 @@ concept_from_mapping('PIH', '7134'),
 concept_from_mapping('PIH', '7646'),
 concept_from_mapping('CIEL', '123'),
 concept_from_mapping('PIH', '7568')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set postnatal_complication = if(diagnosis_concept = 
-concept_from_mapping('PIH', '7252'), 1, 0);
+concept_from_mapping('PIH', '7252'), @yes, @no);
 
 update temp_mch_diagnoses tm set preeclampsia = if(diagnosis_concept in
 (
@@ -198,22 +200,22 @@ concept_from_mapping('PIH', '8354'),
 concept_from_mapping('PIH', '9344'),
 concept_from_mapping('PIH', '47'),
 concept_from_mapping('CIEL', '129251')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set puerperal_infection = if(diagnosis_concept in
 (
 concept_from_mapping('PIH', '9333'),
 concept_from_mapping('PIH', '130')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set spontaneous_abortion = if(diagnosis_concept in
 (
 concept_from_mapping('PIH', '7252'),
 concept_from_mapping('PIH', '7238')
-), 1, 0);
+), @yes, @no);
 
 update temp_mch_diagnoses tm set threatened_abortion = if(diagnosis_concept
-= concept_from_mapping('PIH', '7993'), 1, 0);
+= concept_from_mapping('PIH', '7993'), @yes, @no);
 
 update temp_mch_diagnoses set encounter_location = encounter_location_name(encounter_id);
 update temp_mch_diagnoses set entered_by = encounter_creator_name(encounter_id);
@@ -241,7 +243,7 @@ concept_from_mapping('PIH', '3728'),
 concept_from_mapping('PIH', '2155'),
 concept_from_mapping('CIEL', '112992'),
 concept_from_mapping('CIEL', '120733')
-), 1, 0);
+), @yes, @no);
 
 select 
 	   patient_id,
