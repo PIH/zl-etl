91c5993dc751995b07e185b1d91332b7e4f69c98
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-06-26 16:26:16 +0300

    commit

--- jobs/sql/schemas/oncology_program.sql
+++ jobs/sql/schemas/oncology_program.sql
@@ -1,11 +1,12 @@
-CREATE TABLE oncology_program 
-(
+CREATE TABLE oncology_program (
 emr_id varchar(50),
 program_enrollment_date date,
 program_completion_date date,
-status_start_date date,
-status_end_date date, 
-program_status varchar(30),
-status_date date,
-outcome varchar(100)
+program_outcome varchar(100),
+program_status_start_date date,
+program_status_end_date date,
+program_status varchar(50),
+treatment_status_start_date date,
+treatment_status_end_date date,
+treatment_status varchar(50)
 );
