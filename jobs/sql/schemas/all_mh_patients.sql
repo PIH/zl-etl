1750236e4afa30d84faa1dfda26ed2ed0c2912e9
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-07-16 14:48:22 +0300

    commit

---
+++ jobs/sql/schemas/all_mh_patients.sql
@@ -0,0 +1,10 @@
+CREATE TABLE all_mh_patients
+(
+emr_id varchar(50),
+dob date,
+gender varchar(50),
+town varchar(500),
+referral varchar(500),
+program_enrollment_date date,
+interventions varchar(1000)
+);
