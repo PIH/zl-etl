5c264e735faae45d41a0695e84e20f51755f2342
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-07-26 12:53:49 +0300

    commit

---
+++ jobs/sql/schemas/all_mh_diagnosis.sql
@@ -0,0 +1,10 @@
+CREATE TABLE all_mh_diagnosis 
+(
+emr_id varchar(50),
+encounter_id int,
+encounter_datetime datetime,
+encounter_location_name varchar(50),
+encounter_creator varchar(50),
+provider varchar(50),
+diagnosis varchar(100)
+);
