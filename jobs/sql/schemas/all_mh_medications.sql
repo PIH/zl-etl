05b48348bccaef714e12efd51f12417604b6d619
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-08-31 00:05:32 +0400

    commit

---
+++ jobs/sql/schemas/all_mh_medications.sql
@@ -0,0 +1,12 @@
+CREATE TABLE all_mh_medications 
+(
+emr_id varchar(50),
+encounter_id int,
+encounter_datetime datetime,
+encounter_location_name varchar(50),
+encounter_creator varchar(50),
+provider varchar(50),
+medication_name varchar(500),
+dosage int,
+dosage_unit varchar(50)
+);
