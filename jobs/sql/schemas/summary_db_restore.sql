371122ffe843d7f6270941847352b97046f166fd
dmdesimone <ddesimone@pih.org>
2023-10-26 09:31:26 -0400

    uhm-7482-consolidate-bkup-restore-tables

--- jobs/sql/schemas/summary_db_restore.sql
+++ jobs/sql/schemas/summary_db_restore.sql
@@ -1,4 +1,6 @@
 CREATE TABLE summary_db_restore (
+backup_datetime                              DATETIME,
+restore_datetime                             DATETIME,
 last_restored_patient_datapoint_created_on   DATETIME,
 last_restored_encounter_datapoint_created_on DATETIME,
 last_restored_obs_datapoint_created_on       DATETIME
