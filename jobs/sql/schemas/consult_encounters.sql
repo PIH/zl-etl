420007d4e6bf339446c4d36c1258b1f2fd942da8
dmdesimone <ddesimone@pih.org>
2024-04-21 20:42:24 -0400

    UHM-7881 Create consult_encounters.sql

---
+++ jobs/sql/schemas/consult_encounters.sql
@@ -0,0 +1,18 @@
+create table consult_encounters 
+(                         
+       emr_id             varchar(50),   
+       encounter_id       varchar(50),   
+       visit_id           varchar(50),   
+       encounter_datetime datetime,      
+       user_entered       varchar(255),  
+       datetime_created   datetime,   
+       encounter_location varchar(255),
+       encounter_type     varchar(255),  
+       provider           varchar(255),  
+       trauma             bit,           
+       trauma_type        varchar(255),  
+       return_visit_date  date,          
+       disposition        varchar(255),  
+       index_asc          int,           
+       index_desc         int            
+ );
