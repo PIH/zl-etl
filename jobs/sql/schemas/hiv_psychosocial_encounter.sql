42833aa847180da84a5ccc1a281e3483a08af0f8
aibrahem550 <ahmed.ibrahem550@gmail.com>
2024-06-05 15:22:08 +0300

    commit

---
+++ jobs/sql/schemas/hiv_psychosocial_encounter.sql
@@ -0,0 +1,26 @@
+create table hiv_psychosocial_encounter
+(
+patient_id 			int,
+emr_id				varchar(255),
+encounter_id        int,
+visit_id            int,
+encounter_datetime  datetime,
+datetime_created    datetime,
+user_entered        varchar(255),
+provider            varchar(255),
+return_to_care_follow_up bit,
+undetected_vl_follow_up bit,
+inadherence_to_treatment_follow_up bit,
+other_follow_up bit,
+other_follow_up_text varchar(500),
+home_visit_monitoring bit,
+support_group_monitoring bit,
+food_support_monitoring bit,
+financial_support_monitoring bit,
+income_generator_monitoring bit,
+school_support_monitoring bit,
+other_monitoring bit,
+other_monitoring_text varchar(500),
+index_asc INT,
+index_desc INT
+);
