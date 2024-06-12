2f8f870fbf8c6d2be59e1338abf76b52cb2afb47
dmdesimone <ddesimone@pih.org>
2024-06-12 16:12:14 -0400

    uhm-7788-move-all_admissions-to-separate-job

--- jobs/sql/derivations/update_index_numbers.sql
+++ jobs/sql/derivations/update_index_numbers.sql
@@ -381,43 +381,6 @@ set ce.index_asc = cei.index_asc,
 from consult_encounters ce
 inner join #consult_encounters_indexes cei on cei.encounter_id = ce.encounter_id; 
 
--- Temporary all_admissions_tmp
-
-DROP TABLE IF EXISTS all_admissions;
-create table all_admissions
-(
-   emr_id               varchar(15),
-   encounter_id         varchar(255),
-   visit_id             varchar(255),
-   encounter_type       varchar(50),
-   start_date           datetime,
-   end_date             datetime,
-   creator              varchar(255),
-   date_entered         date,
-   encounter_location   varchar(255),
-   provider             varchar(255),
-   site                 varchar(255),
-   partition_num        int
-);
-
-INSERT INTO all_admissions(emr_id, encounter_id, visit_id, encounter_type, start_date,
-   end_date, creator, date_entered, encounter_location, provider, site, partition_num)
-SELECT emr_id,  
-encounter_id,
-visit_id,
-encounter_type, 
-encounter_datetime  AS start_date,
-lag(encounter_datetime) OVER(PARTITION BY emr_id ORDER BY encounter_datetime desc ) AS end_date, 
-user_entered  AS creator,
-datetime_created  AS date_entered,
-encounter_location,
-provider,
-site,
-partition_num
-FROM adt_encounters ae;
-
-DELETE FROM all_admissions WHERE encounter_type='Sortie de soins hospitaliers';
-
 -- update index asc/desc on hiv_psychosocial_encounter
 drop table if exists #hiv_psychosocial_encounter_indexes;
 select  emr_id, encounter_datetime, encounter_id,
