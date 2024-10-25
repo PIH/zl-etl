589a9f3c7cc6ef65c182a83079cbd94fedf16143
dmdesimone <ddesimone@pih.org>
2024-10-25 14:57:23 -0400

    UHM-8247 add sql indexes to mh_medications script

--- jobs/sql/extractions/mh_medications.sql
+++ jobs/sql/extractions/mh_medications.sql
@@ -25,11 +25,16 @@ FROM encounter e
 WHERE e.encounter_type =@mh_enctype
 AND e.voided =0;
 
+create index temp_encounter_e on temp_encounter(encounter_id);
+
 DROP TABLE IF EXISTS temp_obs;
 CREATE TEMPORARY TABLE temp_obs AS 
 SELECT o.person_id, o.obs_id , o.obs_group_id , o.obs_datetime ,o.date_created , o.encounter_id, o.value_coded, o.concept_id, o.value_numeric , o.voided 
 FROM obs o INNER JOIN temp_encounter te ON te.encounter_id=o.encounter_id 
 WHERE o.voided =0;
+
+create index temp_obs_c1 on temp_obs(encounter_id, concept_id);
+create index temp_obs_c2 on temp_obs(obs_group_id, concept_id);
 
 INSERT INTO all_mh_medications(patient_id,emr_id,encounter_id,obs_group_id,encounter_datetime,encounter_location_name,encounter_creator,provider)
 SELECT 
@@ -44,7 +49,6 @@ encounter_creator(e.encounter_id) encounter_creator,
 provider(e.encounter_id) provider
 FROM temp_encounter e INNER JOIN temp_obs o ON e.encounter_id=o.encounter_id
 AND concept_id=concept_from_mapping('PIH','10742');
-
 
 
 UPDATE all_mh_medications SET medication_name=obs_from_group_id_value_coded_list(obs_group_id,'PIH','10634','en');
