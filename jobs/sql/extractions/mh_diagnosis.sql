dd1922bcfedbc9d7ef590776389971f1151cc212
dmdesimone <ddesimone@pih.org>
2024-12-17 14:01:54 -0500

    UHM-8321 mh_diagnosis ETL failing in humci pipeline

--- jobs/sql/extractions/mh_diagnosis.sql
+++ jobs/sql/extractions/mh_diagnosis.sql
@@ -10,9 +10,9 @@ CREATE TEMPORARY TABLE all_mh_diagnosis
     encounter_id            int,
     encounter_datetime      datetime,
     encounter_location_name varchar(50),
-    encounter_creator       varchar(50),
-    provider                varchar(50),
-    diagnosis               varchar(100)
+    encounter_creator       text,
+    provider                text,
+    diagnosis               varchar(255)
 );
 
 DROP TABLE IF EXISTS temp_encounter;
