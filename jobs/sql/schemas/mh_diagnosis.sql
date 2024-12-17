ae166d6bf5a1e834761f1514ccb690d9f1bca2f1
dmdesimone <ddesimone@pih.org>
2024-12-17 14:01:02 -0500

    UHM-8321 mh_diagnosis ETL failing in humci pipeline

--- jobs/sql/schemas/mh_diagnosis.sql
+++ jobs/sql/schemas/mh_diagnosis.sql
@@ -5,7 +5,7 @@ CREATE TABLE mh_diagnosis
     emr_id                  varchar(50),
     encounter_datetime      datetime,
     encounter_location_name varchar(50),
-    encounter_creator       varchar(50),
-    provider                varchar(50),
-    diagnosis               varchar(100)
+    encounter_creator       text,
+    provider                text,
+    diagnosis               varchar(255)
 );
