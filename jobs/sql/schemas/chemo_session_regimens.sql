99fe71faf273fa537a4e0c29a648bf90092b85f2
dmdesimone <ddesimone@pih.org>
2025-02-26 13:32:48 -0500

    UHM-8441-add-other-regimen-text

--- jobs/sql/schemas/chemo_session_regimens.sql
+++ jobs/sql/schemas/chemo_session_regimens.sql
@@ -2,6 +2,7 @@ CREATE TABLE chemo_session_regimens
 (emr_id                    varchar(25),   
 obs_id                     varchar(25),  
 encounter_id               varchar(25),
+encounter_datetime         datetime,
 chemo_regimen_name         varchar(255),
 other_regimen              text
 );
