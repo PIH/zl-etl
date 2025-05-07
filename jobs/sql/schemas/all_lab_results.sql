44a8a144837e0bbd9c26c67396316192b1f5faea
dmdesimone <ddesimone@pih.org>
2025-05-07 15:28:23 -0400

    SL-945 standardize ZL and SL lab results reporting

--- jobs/sql/schemas/all_lab_results.sql
+++ jobs/sql/schemas/all_lab_results.sql
@@ -1,6 +1,7 @@
 CREATE TABLE all_lab_results
 (
     lab_obs_id               VARCHAR(50),
+    patient_id               VARCHAR(50),
     emr_id                   VARCHAR(50),
     visit_id                 VARCHAR(50),
     encounter_id             VARCHAR(50),
@@ -10,11 +11,6 @@ CREATE TABLE all_lab_results
     unknown_patient          VARCHAR(50),
     gender                   VARCHAR(11),
     age_at_enc               FLOAT,
-    department               VARCHAR(255),
-    commune                  VARCHAR(255),
-    section                  VARCHAR(255),
-    locality                 VARCHAR(255),
-    street_landmark          VARCHAR(255),
     order_number             VARCHAR(50),
     orderable                VARCHAR(255),
     test                     VARCHAR(255),
