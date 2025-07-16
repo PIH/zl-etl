adf697a31945a942ddb7f0f5bfeb4d7754a7b098
dmdesimone <ddesimone@pih.org>
2025-07-15 21:59:16 -0400

    UHM-8689-Add-patient_program_id-to-hiv_visit-and-pmtct_visit-tables

--- jobs/sql/schemas/hiv_patient_program.sql
+++ jobs/sql/schemas/hiv_patient_program.sql
@@ -1,6 +1,6 @@
 create table hiv_patient_program
 (
-    patient_program_id INT,
+    hiv_program_id INT,
     emr_id             VARCHAR(255),
     date_enrolled      DATE,
     date_completed     DATE,
